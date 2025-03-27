e201bf3598b224387d340460c18509d2c9ee638e
Daniel Guimarães de Faria <164964227+Daniel-Guimaraes-de-Faria@users.noreply.github.com>
2025-03-27 23:19:15 +0000

    Ajuste no cadastro

--- projeto/src/app/cadastro/cadastro.component.ts
+++ projeto/src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,10 +10,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
+  cadastroForm: FormGroup;
   nome: string = '';
   categoria: string = '';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {
+    this.cadastroForm = this.fb.group({
+      nome: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ativo: [false],
+      tipo: ['', Validators.required]
+    });
+  }
 
   salvar() {
     if (this.nome && this.categoria) {
